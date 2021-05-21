36258b459d2dff00a9a23f9767a9d094f381bccc
gnoose <jieli3838@gmail.com>
2021-05-21 13:37:31 -0400

    wip: work on chat page implementation

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ChatRoutingModule } from './chat-routing.module';
-import { ChatComponent } from './chat.component';
+import { UiKitModule } from '../ui-kit/ui-kit.module';
 
+import { ChatRoutingModule } from './chat-routing.module';
+
+import { ChatComponent } from './chat.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { MessageSectionComponent } from './message-section/message-section.component';
 
 @NgModule({
   declarations: [
-    ChatComponent
+    ChatComponent,
+    ContactListComponent,
+    MessageSectionComponent
   ],
   imports: [
     CommonModule,
-    ChatRoutingModule
+    ChatRoutingModule,
+    UiKitModule
   ]
 })
-export class ChatModule { }
+export class ChatModule {
+}
---
+++ src/app/ui-kit/avatar/avatar.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'auto-market-avatar',
+  templateUrl: './avatar.component.html',
+  styleUrls: ['./avatar.component.scss']
+})
+export class AvatarComponent implements OnInit {
+
+  @Input() src = '';
+  @Input() size = 60;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
