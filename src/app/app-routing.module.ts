a41d5a681a07291847ad0556814901977baf4e28
gnoose <jieli3838@gmail.com>
2021-05-21 08:31:06 -0400

    feat: build basic structure of the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from './layout/layout.component';
+
+const routes: Routes = [
+  {
+    path: '', component: LayoutComponent,
+    children: [
+      {
+        path: '', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'auto-market-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
